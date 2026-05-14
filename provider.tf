3dd16d530515c23d1639377347e47536103b7475
rlfrjs3 <rlfrjs3@naver.com>
2026-05-14 15:14:51 +0900

    20260514

--- provider.tf
+++ provider.tf
@@ -29,14 +29,14 @@ terraform {
 #공통 리전
 provider "aws" {
   region = var.region
-  profile = "stephane"
+#  profile = "stephane"
 }
 
 #cloudfront에 ACM SSL 인증서 연결을 위해서는 us-east-1 리전에서 인증서를 발급해야 함(인증서를 해당 리전에서 중앙 관리하기 때문에)
 provider "aws" {
   alias = "us_east_1"
   region = "us-east-1"
-  profile = "stephane"
+#  profile = "stephane"
 }
 
 
